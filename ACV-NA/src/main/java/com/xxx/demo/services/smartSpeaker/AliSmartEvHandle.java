0547c20c519fc061a249c2cc2c963da36209a13b
65499 <654998538@qq.com>
2019-02-18 10:47:33 +0800

    消息中心

--- ACV-NA/src/main/java/com/xxx/demo/services/smartSpeaker/AliSmartEvHandle.java
+++
@@ -1,8 +0,0 @@
-package com.xxx.demo.services.smartSpeaker;
-
-import com.alibaba.fastjson.JSONObject;
-import com.xxx.demo.models.jsonBean.smartSpeaker.TaskRequest;
-
-public interface AliSmartEvHandle {
-    JSONObject execute(TaskRequest taskRequest);
-}
